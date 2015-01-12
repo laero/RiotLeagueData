087ba6cb3d936deae02c8cc1a8a3ee1f392147b2
laero <filoticjunction@gmail.com>
2015-01-12 02:26:10 -0600

    Setup example formatting and skeleton for project. getChampions has been implemented.

--- example.js
+++ example.js
@@ -1,15 +1,17 @@
 
 //Rudimentary 
-
 var lolData = require("./lib/loldata");
 
 lolData.initialize('key-goes-here');
 
 lolData.getChampions(true, function(err, data) {
+
 	if (!err) {
 		console.log(data);
 	}
 	else {
 		console.log("Error code: " + err.status + " " + err.status.message);
 	}
+
+	console.log(data);
 });
